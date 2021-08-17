5519fbd4fd7c87c83229929918aa903fd37b1c6a
Admin <Admin@DESKTOP-3A2Q46S>
2021-08-17 11:07:30 +0530

    Sales class updated for array length

--- firstdemo/src/main/java/firstdemo/Sales.java
+++ firstdemo/src/main/java/firstdemo/Sales.java
@@ -3,9 +3,10 @@ package firstdemo;
 public class Sales {
 	public void display() {
 		int[] arr = {23,45,62,65};
-		for(int num:arr) {
-			System.out.println(num);
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
 		}
+		System.out.println("Size of array: "+ arr.length);
 	}
 	public void print() {
 		System.out.println("Print all array numbers");
